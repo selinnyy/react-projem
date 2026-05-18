a5681840c89e4020d0d97761268bd527674c139e
selinnyy <25108019@samsun.edu.tr>
2026-05-18 14:49:51 +0300

    ilk yukleme

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>İlk React Projem Başarıyla Çalıştı! 🎉</h1>
+      <p>Bu satırı VS Code üzerinden düzenledim.</p>
+    </div>
+  )
+}
+
+export default App
